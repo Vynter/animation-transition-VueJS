5cc9a4624a697de4ff9a4c4526578b07d4cce76b
Vynter <amine-cheraitia@hotmail.com>
2020-11-20 17:58:58 +0100

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app" class="container">
-    <transition name="qqq" appear appear-class="qqq">
-      <img alt="Vue logo" src="./assets/logo.png" />
+    <transition name="qqq" appear>
+      <div  v-if="okk">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+          magni vel a laudantium aperiam sunt quam officia expedita. Nemo
+          doloremque iste a magni minus ab animi amet perspiciatis expedita
+          eaque?
+        </p>
+      </div>
       <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     </transition>
+    <button @click="okk = !okk">tog</button>
     <Alertz></Alertz>
   </div>
 </template>
@@ -17,26 +26,37 @@ export default {
     // HelloWorld,
     Alertz,
   },
+  data() {
+    return {
+      okk: true,
+    };
+  },
 };
 </script>
 
 <style>
-  #app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
 
-} 
-
-.qqq-enter-actove,
+.qqq-enter-active,
 .qqq-leave-active {
   transition: all 1s;
 }
-
+.qqq-leave {
+  transform: translateX(20px);
+}
 .qqq-enter,
 .qqq-leave-active {
   opacity: 0;
   transform: translateX(20px);
+}
+
+.qqq-move {
+  transition: transform 1s;
+  transform: translateY(-120px);
 }
 </style>
